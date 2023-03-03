86a2d3ebba6729058179a11b4249be7e171ddc4d
chiragrajbr <115540327+chiragrajbr@users.noreply.github.com>
2023-03-03 18:30:27 +0530

    math,faf

---
+++ ES6/faf.js
@@ -0,0 +1,20 @@
+//fat arrow function - it reduces the code
+let a=()=>{
+    console.log("fat arrow function")
+}
+a()
+
+// passing parameters
+let b=(a,b)=>{
+    console.log(a+b)
+}
+b(50,40)
+
+let c=()=>{
+    return "hello"
+}
+console.log(c())
+
+
+
+
---
+++ ES6/math.js
@@ -0,0 +1,5 @@
+//math method - auto generate numbers
+//every time values gonna change
+console.log(Math.random())  //basic generates
+console.log(Math.random()*10)  //multiple generates
+console.log(Math.round(Math.random()*10))  //round off  generates
